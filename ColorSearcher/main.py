0279df67eea08c63fe7561f38495d17c420518ed
Brendan Moore <brendanm42@gmail.com>
2018-11-23 12:43:23 -0500

    frameworking

---
+++ ColorSearcher/main.py
@@ -0,0 +1,21 @@
+"""
+date: Nov 11, 23
+author: @BrendanMoore42
+
+Image to Hex colours
+
+Feed in an image and returns a list of the Hex colour values and names for that colour.
+"""
+import sys
+import numpy as np
+
+image = url
+
+def main(image):
+
+
+
+
+
+if __name__ == '__main__':
+    main()
