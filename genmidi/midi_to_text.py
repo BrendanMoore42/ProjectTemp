5b0ccaf71716c5ee8151edd1f2d9837eef3ac79e
Brendan Moore <brendanm42@gmail.com>
2018-12-01 20:22:07 -0500

    testing midi formats

--- genmidi/midi_to_text.py
+++ genmidi/midi_to_text.py
@@ -4,19 +4,48 @@ date: November 30, 2018
 
 Converts a Midi file to text. And perhaps back again.
 """
-# import textgenrnn
-import py_midicsv as mc
-# Load midi file
-midi = mc.midi_to_csv("C:/Users/B/Desktop/midis/Nintendo/DKR/DKRDrago.mid")
+import sys
+from mido import MidiFile
+# from textgenrnn import textgenrnn
 
-print(midi.readlines())
+mid = MidiFile("C:/Users/B/Desktop/midis/Nintendo/Misc/airfortress.mid")
+temp_midi = []
+
+for i, track in enumerate(mid.tracks):
+
+    print(f'¨{i}: {track}')
+
+    # file = open('midi_txt_test1.txt', 'w+')
+    for msg in track:
+
+        # temp_midi.append(str(msg))
+        print('\t\t', str(msg))
+
+# with open('midi_txt_test1.txt', 'w') as f:
+#     f.writelines(temp_midi)
+
+# textgen = textgenrnn()
+#
+# # textgen.train_from_file
+# textgen.generate(string)
 
 
-# Parse the CSV output of the previous command back into a MIDI file
-midi_object = mc.csv_to_midi(midi)
 
-# Save the parsed MIDI file to disk
-with open("example_converted.mid", "wb") as output_file:
-    midi_writer = mc.FileWriter(output_file)
-    midi_writer.write(midi_object)
-
+#
+#
+#
+# import py_midicsv as mc
+# # Load midi file
+# midi = mc.midi_to_csv("C:/Users/B/Desktop/midis/Nintendo/DKR/DKRDrago.mid")
+#
+# print(midi.readlines())
+#
+#
+# # Parse the CSV output of the previous command back into a MIDI file
+# midi_object = mc.csv_to_midi(midi)
+#
+# # Save the parsed MIDI file to disk
+# with open("example_converted.mid", "wb") as output_file:
+#     midi_writer = mc.FileWriter(output_file)
+#     midi_writer.write(midi_object)
+#
