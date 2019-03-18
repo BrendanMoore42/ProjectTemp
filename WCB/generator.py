e8707ebc5cb028c1d1f77c1f41ef2fb6efcfe8d9
BrendanMoore42 <brendanm42@gmail.com>
2019-03-18 09:51:03 -0400

    Adding logs

--- WCB/generator.py
+++ WCB/generator.py
@@ -10,41 +10,42 @@ get a daily allowance for each behaviour. Calculates off weekly basis.
 Takes each habit and returns on the odds you can do that that day and how much.
 """
 import sys
+import json
 import random
+import datetime
 
 
 class Habit(object):
-    def __init__(self, habit, frequency=0.0, weekend=False):
+    def __init__(self, habit, frequency=0.0):
         self.habit = habit
-        self.daily = 0.0
-        self.weekly = 0.0
-        self.monthly = 0.0
-        self.weekend = weekend
+        self.nickname = self.habit[0]
+        self.level = self.habit[1]
+        self.weekly = self.habit[2]
+        self.weekend = self.habit[3]
         self.frequency = frequency
 
-        self.calc_frequency()
 
-        print(self.frequency)
+        # self.calc_frequency()
+
+        # print(self.frequency)
         print(self.habit)
+        print(f'Nickname: {self.nickname}, Level: {self.level}, Weekly: {self.weekly}')
 
 
-    def calc_frequency(self):
-
-
-        if self.frequency:
-            return self.frequency
-        if self.weekend:
-            max_value = 2
-
-        weekly_odds = [1] * int(self.weekly)
-        while len(weekly_odds) != 7:
-            weekly_odds.append(0)
-        print(weekly_odds)
-        frequency = random.choice([0,1])
-        return frequency
-
-
-
+    # def calc_frequency(self):
+    #
+    #
+    #     if self.frequency:
+    #         return self.frequency
+    #     if self.weekend:
+    #         max_value = 2
+    #
+    #     weekly_odds = [1] * int(self.weekly)
+    #     while len(weekly_odds) != 7:
+    #         weekly_odds.append(0)
+    #     print(weekly_odds)
+    #     frequency = random.choice([0,1])
+    #     return frequency
 
 
 
@@ -57,9 +58,10 @@ def main():
 
         print(freqs)
 
-    freqs = list(zip(freqs[::2], freqs[1::2]))
+    # freqs = list(zip(freqs[::2], freqs[1::2]))
 
     for habit in freqs:
+        print(habit)
         habit = Habit(habit)
 
 
